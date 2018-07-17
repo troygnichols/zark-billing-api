2e4969d96d39c3b88838ba52aa025ee978c85809
Troy Nichols <troygnichols@gmail.com>
2018-07-17 18:24:04 -0400

    Update db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160310010203) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "invoices", force: true do |t|
     t.string   "entity_name"
     t.string   "client_name"
