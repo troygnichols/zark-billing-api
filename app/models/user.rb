0e10f85c373d7944a50f913c3bdf20573766bee1
Troy Nichols <troygnichols@gmail.com>
2018-07-27 12:34:42 -0400

    Validate email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
   validates :password, presence: true,
     length: {within: 6..100}, allow_nil: true
 end
