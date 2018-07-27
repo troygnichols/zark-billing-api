cec879dfab540c2b9b250fc4fb4f9a118c3bcbf1
Troy Nichols <troygnichols@gmail.com>
2018-07-27 18:07:57 -0400

    More invoice validations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,7 @@ class Invoice < ApplicationRecord
   accepts_nested_attributes_for :items, allow_destroy: true
 
   validates_uniqueness_of :invoice_id, scope: :client_name
+  validates_presence_of :invoice_id
+  validates_presence_of :client_name
+  validates_presence_of :entity_name
 end
