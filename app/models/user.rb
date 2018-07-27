c72dbe90d87280e22c66096c8c039df697bc272b
Troy Nichols <troygnichols@gmail.com>
2018-07-26 22:41:55 -0400

    Require longer password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, uniqueness: true
+  validates :password, presence: true,
+    length: {within: 6..100}, allow_nil: true
 end
