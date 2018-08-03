b9a78e4c739f33ee04edc0fb8bf206dcb91cd99b
Troy Nichols <troygnichols@gmail.com>
2018-08-03 01:14:09 -0400

    Add production mail default url config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'https://zark-billing.herokuapp.com', protocol: 'https' }
+
   config.action_mailer.smtp_settings = {
     address:        'smtp.elasticemail.com',
     port:           2525,
