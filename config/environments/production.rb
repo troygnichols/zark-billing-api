76851cad370c10b1c7626b8c76c06b58082b6c84
Troy Nichols <troygnichols@gmail.com>
2018-08-03 01:09:54 -0400

    Production mail config: elastic email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,13 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
-
+  config.action_mailer.smtp_settings = {
+    address:        'smtp.elasticemail.com',
+    port:           2525,
+    user_name:      ENV['SMTP_USER'],
+    password:       ENV['SMTP_PASSWORD'],
+    authentication: 'plain',
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
