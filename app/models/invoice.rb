4d72eca4dfcfb0c6726a9f69fec7b0762b2029d7
Troy Nichols <troygnichols@gmail.com>
2018-08-05 00:08:57 -0400

    Update uniquness validation

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,7 +5,8 @@ class Invoice < ApplicationRecord
 
   accepts_nested_attributes_for :items, allow_destroy: true
 
-  validates_uniqueness_of :invoice_id, scope: :client_name
+  validates_uniqueness_of :invoice_id, scope: [:client_name, :user_id],
+    message: 'ID already used'
   validates_presence_of :invoice_id
   validates_presence_of :client_name
   validates_presence_of :entity_name
