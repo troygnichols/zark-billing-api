6b647f16c592321e0539642e77846f0503909d97
Troy Nichols <troygnichols@gmail.com>
2018-08-05 11:16:23 -0400

    Validate email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,9 @@ class User < ApplicationRecord
 
   has_many :invoices
 
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true, format: {
+    with: URI::MailTo::EMAIL_REGEXP
+  }
   validates :password, presence: true,
     length: {within: 6..100}, allow_nil: true
 
